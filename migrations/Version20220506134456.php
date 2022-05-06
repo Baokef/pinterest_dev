5ddce7a42eacf6485d07870e466983d99c240802
kefrn Bao <keprhen88@gmail.com>
2022-05-06 16:23:24 +0200

    9 commit

---
+++ migrations/Version20220506134456.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please m odify to your needs!
+ */
+final class Version20220506134456 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'ajout de createdAt et updatedAt dans pins';
+    }
+
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pins ADD created_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pins DROP created_at');
+    }
+}
