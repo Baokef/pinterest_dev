4f40344f4467a782f6cdeea8f5bdc8023f49b2ba
kefrn Bao <keprhen88@gmail.com>
2022-09-14 13:30:03 +0200

    un utiliseur ne peut modifier que les pins qui lui appartiennent

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -24,35 +24,17 @@ class PinController extends AbstractController
      */
     public function index(PinRepository $repo): Response
     {
-        // $pin1 = new Pin;
-        // $pin2 = new pin;
-        // $pin3 = new Pin;
-        // $pin4 = new pin;
-        // $pin5 = new Pin;
 
-        // $pin1->setTitle("Pin 1");
-        // $pin1->setDescription("Description Pin 1");
-        // $pin2->setTitle("Pin 2");
-        // $pin2->setDescription("Description Pin 2");
-        // $pin3->setTitle("Pin 3");
-        // $pin3->setDescription("Description Pin 3");
-        // $pin4->setTitle("Pin 4");
-        // $pin4->setDescription("Description Pin 4");
-        // $pin5->setTitle("Pin 5");
-        // $pin5->setDescription("Description Pin 5");
 
-        // $em = $this->getDoctrine()->getManager();
-        // $em->persist($pin1);
-        // $em->persist($pin2);
-        // $em->persist($pin3);
-        // $em->persist($pin4);
-        // $em->persist($pin5);
+        if ($this->getUser()) {
+            if (!$this->getUser()->isVerified()) {
+                $this->addFlash('info', 'Your email address is not verified.');
+            }
+        }
 
 
 
-
-        //$em->flush();
-
+        
         return $this->render('pin/index.html.twig', ['pins' => $repo->findAll()]);
     }
     /**
